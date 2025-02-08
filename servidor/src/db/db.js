ae01fa42bc148fc367e94918f402bd9c6ee4db0c
Juan Jose <168502269+Jj-Bit27@users.noreply.github.com>
2025-02-07 21:45:48 -0600

    Update db.js

--- servidor/src/db/db.js
+++ servidor/src/db/db.js
@@ -1,9 +1,9 @@
 import { createPool } from 'mysql2/promise'
 
 export const pool = createPool({
-  host: 'boy8ak6ctezq7asppmf5-mysql.services.clever-cloud.com',
-  port: 3306,
-  user: 'uy2i461pdkciabne',
-  password: 'ApMfpUsmaeQnK5z81POZ',
-  database: 'boy8ak6ctezq7asppmf5'
+  host: 'host',
+  port: 1111,
+  user: 'usuario',
+  password: 'contrase',
+  database: 'bbase de datos'
 })
